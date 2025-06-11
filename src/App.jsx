52ee4090cf7cf9e44467cbf1748fd6002ff6b2d4
amankolhe <amankolhe3@gmail.com>
2025-06-11 16:23:24 +0530

    frontend added

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+    const [usersData, setusersData] = useState([]);
+    const getData = async () => {
+        const res = await axios.get("http://3.110.166.9:5001/api/users");
+        setusersData(res.data.users);
+    };
+    useEffect(() => {
+        getData();
+    }, []);
+    return (
+        <div>
+            <h1>Test 1</h1>
+            <h1>List of users</h1>
+            {usersData.map((user) => (
+                <div>
+                    <p>{user.name}</p>
+                    <p>{user.email}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+export default App;
